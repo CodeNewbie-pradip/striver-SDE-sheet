cab5b214795c51cd78e0f8f96f8f145be4a4c48a
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-09-04 18:49:01 +0530

    Leetcode 56

---
+++ Day 2 Arrays II/Merge Intervals.cpp
@@ -0,0 +1,36 @@
+/*
+Leetcode Link               : https://leetcode.com/problems/merge-intervals/submissions/
+Leetcode difficulty         : Medium
+Leetcode Related Topic      : Array, Sorting
+*/
+
+//------------------------------Solution - in - C++ -----------------------------------------------------------
+
+//Approach 1: Brute Force
+//T.C       : O(n)
+//S.C       : O(1)
+//-------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()<=2){
+            return intervals;
+        }
+        sort(intervals.begin(), intervals.end());
+        int i=1;
+        
+        while(i<intervals.size()){
+            
+            if(intervals[i-1][1]>=intervals[i][0]){
+                intervals[i-1][1]=max(intervals[i-1][1], intervals[i][1]);
+                intervals.erase(intervals.begin()+i);
+            }else{
+                i++;
+            }
+        }
+        return intervals;
+    }
+};
+
+//--------------------------------------------------------------------------------------------------
